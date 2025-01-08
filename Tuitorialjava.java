c19b08c3e64e7f783a9746f135b5583a1144828c
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2025-01-08 09:11:10 +0530

    Create Tuitorialjava.java

---
+++ Tuitorialjava.java
@@ -0,0 +1,13 @@
+// File name and main function name have been same .
+// just a example
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Main {
+   public Main() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("hello world ");           //here println is use for go to the next line.
+      System.out.println("i am going to start java");
+      System.out.print("i am Labani Haldar");
+   }
+}
