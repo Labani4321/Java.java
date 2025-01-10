bafb3bce6264e1d2ae63dfde07f6b980b2ff61d0
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2025-01-10 11:03:17 +0530

    Update Java.java

--- Java.java
+++ Java.java
@@ -26,3 +26,35 @@ solve.num2 = sc.nextInt();
 solve.num3 = sc.nextInt();
 sc.close();
 System.out.println("So the Biggest Number is :" + solve.printresult()); }
+
+// 3. conditons java 
+import java.util.*;
+public class Condition {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt ();
+        
+        if (age >18) {
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Not adult");
+            }
+        }
+    }
+
+// Even or odd check 
+import java.util.*;
+public class Condition {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt ();
+        
+        if (num % 2 ==0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+            }
+        }
+    }
